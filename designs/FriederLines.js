8304f407a7a52ceabc54644b90976c0f60641fb3
Nitin <tulswani19@gmail.com>
2018-08-11 12:52:32 +0530

    New API for Frieder lines component

--- designs/FriederLines.js
+++ designs/FriederLines.js
@@ -1,67 +1,113 @@
 import React from 'react'
-import P5 from 'p5'
 import PropTypes from 'prop-types'
+import Two from 'two.js'
 
-const drawFriederLines = (instance, props) => {
-  const drawLines = () => {
-    instance.translate(20, -20)
-    instance.scale(props.scale)
+export class FriederLines extends React.Component {
+  static defaultProps = {
+    strokeVert: 'mistyrose',
+    strokeHor: 'pink',
+    bg: '#2d121a',
+    scaleOffset: 0.08,
+    rotationOffset: 4,
+    callback: ctrl => {}
+  }
+
+  static propTypes = {
+    scale: PropTypes.number,
+    strokeVert: PropTypes.string,
+    strokeHor: PropTypes.string,
+    scaleOffset: PropTypes.number,
+    rotationOffset: PropTypes.number
+  }
+
+  // Main TwoJS instance
+  TwoJS = null
+
+  componentDidMount() {
+    const container = document.getElementById('frieder')
+    const params = { width: 520, height: 550 }
+
+    this.TwoJS = new Two(params).appendTo(container)
+    this.props.callback(this.TwoJS)
+
+    // flush the rendered lines with shouldDequeue flag set to false because we don't want to clear the current batch of lines
+    this.flush({ shouldDequeue: false })
+  }
+
+  startAnimation = (lines, { scaleOffset, rotationOffset }) => {
+    lines.forEach(line => {
+      this.TwoJS.bind('update', frames => {
+        // This will be called on every 2N frame
+        if (frames % 2 === 0) {
+          if (line.scale > 0.9999) {
+            line.scale = line.rotation = 0
+          }
+
+          const t = (1 - line.scale) * scaleOffset
+
+          line.scale += t
+          line.rotation += t * rotationOffset * Math.PI
+        }
+      }).play()
+    })
+  }
+
+  componentDidUpdate() {
+    // An update occurred. So restart from scratch and clear the current batch of lines
+    this.flush({ shouldDequeue: true })
+  }
+
+  drawLines = (instance, props) => {
+    const renderedLines = []
 
     for (let x = 20; x <= 85; x += 5) {
       if (x <= 50) {
-        instance.stroke(props.strokeVert)
-        instance.line(x, 20, x, 85)
+        const line1 = instance.makeLine(x, 20, x, 85)
+        line1.stroke = props.strokeVert
+        line1.linewidth = 0.25
+
+        const group = instance.makeGroup(line1)
+        group.scale = 6
+        renderedLines.push(line1)
       }
     }
 
     for (let y = 50; y <= 115; y += 5) {
       if (y <= 80) {
-        instance.stroke(props.strokeHor)
-        instance.line(15, y, 70, y)
+        const line2 = instance.makeLine(15, y, 70, y)
+        line2.stroke = props.strokeHor
+        line2.linewidth = 0.25
+
+        const group = instance.makeGroup(line2)
+
+        group.scale = 6
+        renderedLines.push(line2)
       }
     }
+
+    return renderedLines
   }
 
-  return drawLines
-}
+  flush = ({ shouldDequeue }) => {
+    // Clear the current batch of lines and restart from scratch
+    if (shouldDequeue) {
+      this.TwoJS && this.TwoJS.clear()
+    }
 
-export class FriederLines extends React.Component {
-  wrapper = React.createRef()
+    const renderedLines = this.drawLines(this.TwoJS, this.props)
 
-  static defaultProps = {
-    width: 500,
-    height: 500,
-    strokeVert: 'mistyrose',
-    strokeHor: 'pink',
-    scale: 1
+    this.startAnimation(renderedLines, this.props)
+
+    this.TwoJS.update()
   }
-
-  static propTypes = {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    scale: PropTypes.number,
-    strokeVert: PropTypes.string,
-    strokeHor: PropTypes.string
-  }
-
-  componentDidMount() {
-    this.renderLines(this.props)
-  }
-
-  renderLines = props =>
-    new P5(sketch => {
-      const drawLines = drawFriederLines(sketch, props)
-
-      sketch.setup = function() {
-        sketch.createCanvas(props.width, props.height)
-      }
-
-      sketch.draw = function() {
-        drawLines()
-      }
-    }, this.wrapper.current)
 
   render() {
-    return <div ref={this.wrapper} />
+    const styles = {
+      background: this.props.bg,
+      display: 'inline-block',
+      margin: '20px auto'
+    }
+
+    return <div id="frieder" style={styles} />
   }
 }
--- index.js
+++ index.js
@@ -5,6 +5,39 @@ import { StarFractal } from './designs/Star'
 import { FriederLines } from './designs/FriederLines'
 import { WavyLines } from './designs/WavePatternLines'
 
-const App = () => <WavyLines scale={4} up={50} down={180} />
+class App extends React.Component {
+  state = {
+    value: 4,
+    instance: null
+  }
 
+  handleInput = e => {
+    const value = parseInt(e.target.value)
+    this.setState({ value })
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={this.state.value}
+          onChange={this.handleInput}
+        />
+        <button onClick={e => this.state.instance.pause()}>Pause</button>
+        <button onClick={e => this.state.instance.play()}>Play</button>
+        <FriederLines
+          scaleOffset={0.08}
+          rotationOffset={this.state.value}
+          width={500}
+          height={500}
+          strokeVert="purple"
+          callback={instance => this.setState({ instance })}
+        />
+      </div>
+    )
+  }
+}
 ReactDOM.render(<App />, document.getElementById('root'))
