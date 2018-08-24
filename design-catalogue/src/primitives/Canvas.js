47315e4d18d6c4cd2c53759e0db8d4b0853fc489
Nitin <tulswani19@gmail.com>
2018-08-24 16:35:53 +0530

    updated project structure

--- design-catalogue/src/primitives/Canvas.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-// This is required to paint the svg onto the canvas and then make an octet stream using `toDataURI()` on the canvas element for downloading the design
-export const ShadowCanvas = ({ id }) => (
-  <canvas id={id} width="1000px" height="600px" style={{ display: 'none' }} />
-)
--- designs-core/utils/index.js
+++
@@ -1,2 +0,0 @@
-export const random = (max, min) =>
-  Math.floor(Math.random() * (max - min) + min)
